919ddbb6d3bdb616ee9e6f05a4b91a889039787a
FernandoChaves <fernando.chaves16@hotmail.com>
2021-04-25 20:41:09 -0300

    readjusted files to styled components

---
+++ src/components/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #DF3A40;
+  color: #ffffff;
+`;
+
+export const TextHome = styled.Text`
+  color: #fff;
+  font-size: ${32}px;
+`;
